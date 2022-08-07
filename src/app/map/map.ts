eb356083cf430ac5d985ee027670a1b34eac40f2
Yurii <yurii.nadilny@nerdysoft.com>
2022-08-07 14:05:36 +0300

    Init commit

---
+++ src/app/map/map.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.html',
+})
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

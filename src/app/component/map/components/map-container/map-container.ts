dae7c790c91b03d9b4103c2f2a10427c4b7145e3
Yurii <y.nadilnyi@gmail.com>
2022-08-09 22:51:12 +0300

    Set map style

--- src/app/component/map/components/map-container/map-container.ts
+++ src/app/component/map/components/map-container/map-container.ts
@@ -1,5 +1,7 @@
+import { MapTypeStyle } from '@agm/core';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MarkerService } from 'src/app/providers/marker';
+import { styles } from './stylse/style';
 
 @Component({
   selector: 'm4p-map-container',
@@ -11,6 +13,7 @@ export class MapContainerComponent implements OnInit {
   readonly zoom: number = 4;
   selectedMarker!: string;
   isLoading: boolean = true;
+  styles: MapTypeStyle[] = styles;
 
   constructor(
     public marker: MarkerService
